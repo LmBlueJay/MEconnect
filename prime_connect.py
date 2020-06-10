f50ff8eea8aa1c92fa77e9035e1171d35ffcfb76
LmBlueJay <matthew.crowley4@gmail.com>
2020-06-10 17:42:17 -0400

    Created Pseudocode and project objective

---
+++ prime_connect.py
@@ -0,0 +1,20 @@
+
+# Connect with myEducator database
+
+# import the msql.connector
+
+# enter access credentials 
+
+# use the cursor.execute command to write mySQL code
+
+# VERSION 1
+
+# I want to use python to asses mySQL database revancy based on when they were last used
+
+# The objective of this script is to use python to:
+    # Prompt the user for a threshold time
+    #/ Create a table of column names in a specific schema which contain a timestamp or time-related datatype
+    # Use each row of this table to create a mySQL query that will determine the relevance of the row time to the user-specified threshold time
+    # The schema, table, columname are displayed for column names which qualify 
+    # This final table is displayed 
+
